58e326b6282a5acbe7b8a2c814b53d9d5a1752a2
virtualarchitectures <virtualarchitectures@gmail.com>
2025-10-02 12:11:30 -0400

    Added class bash scripts

---
+++ 04_Secure_Programming/Lecture_2/Bash_Scripts/studentchat.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Hi, how was your homework?"
+
+sleep 3
+
+echo "Hi, I found it pretty easy."
+
+sleep 3
+
+echo "I actually found it quite hard. What is your secret?"
+
+sleep 3
+
+echo "I turn up to lectures. See ya!"
+
+sleep 3
+
+echo "Good advice! Bye!"
